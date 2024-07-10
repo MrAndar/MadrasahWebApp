ca05ca7080758865d88da87e06620b24d9f610c2
MrAndarCS <r34p3r09@gmail.com>
2024-07-10 02:30:23 +0100

    Added CRUD functionality to attendance app

--- attendance/views.py
+++ attendance/views.py
@@ -1,5 +1,8 @@
+# attendance/views.py
+from datetime import date
+
 from django.views import View
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse_lazy
@@ -36,6 +39,54 @@ class AttendanceListView(LoginRequiredMixin, ListView):
             classes[class_name][record.profile].append(record)
 
         context['classes'] = classes
+        return context
+
+
+class AttendanceDetailView(LoginRequiredMixin, ListView):
+    model = Attendance
+    template_name = 'attendance/attendance_detail.html'
+    context_object_name = 'attendance_records'
+
+    def get_queryset(self):
+        user_profile = self.request.user.profile
+        if user_profile.user_type == 'admin_teacher':
+            return Attendance.objects.filter(profile__id=self.kwargs['profile_id']).order_by('-date')
+        elif user_profile.user_type == 'regular_teacher':
+            return Attendance.objects.filter(profile__id=self.kwargs['profile_id'],
+                                             profile__class_name__teacher=user_profile).order_by('-date')
+        else:
+            return Attendance.objects.none()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        profile = get_object_or_404(Profile, id=self.kwargs['profile_id'])
+        attendance_records = self.get_queryset()
+
+        present_count = attendance_records.filter(status='P').count()
+        absent_count = attendance_records.filter(status='A').count()
+        late_count = attendance_records.filter(status='L').count()
+
+        # Calculate total possible attendances
+        user_created_date = profile.user.date_joined.date()
+        today = date.today()
+        total_possible_attendances = (today - user_created_date).days
+
+        # Calculate attendance and punctuality percentages
+        if total_possible_attendances > 0:
+            attendance_percentage = (present_count / total_possible_attendances) * 100
+            punctuality_percentage = ((present_count + late_count) / total_possible_attendances) * 100
+        else:
+            attendance_percentage = 0
+            punctuality_percentage = 0
+
+        context['profile'] = profile
+        context['present_count'] = present_count
+        context['absent_count'] = absent_count
+        context['late_count'] = late_count
+        context['total_possible_attendances'] = total_possible_attendances
+        context['attendance_percentage'] = attendance_percentage
+        context['punctuality_percentage'] = punctuality_percentage
+
         return context
 
 
@@ -76,3 +127,20 @@ class TakeAttendanceView(LoginRequiredMixin, View):
 
         return render(request, self.template_name,
                       {'formset': formset, 'class_instance': class_instance, 'form_student_pairs': form_student_pairs})
+
+
+class AttendanceUpdateView(LoginRequiredMixin, UpdateView):
+    model = Attendance
+    fields = ['date', 'status', 'reason']
+    template_name = 'attendance/attendance_form.html'
+
+    def get_success_url(self):
+        return reverse_lazy('attendance-detail', kwargs={'profile_id': self.object.profile.id})
+
+
+class AttendanceDeleteView(LoginRequiredMixin, DeleteView):
+    model = Attendance
+    template_name = 'attendance/attendance_confirm_delete.html'
+
+    def get_success_url(self):
+        return reverse_lazy('attendance-detail', kwargs={'profile_id': self.object.profile.id})
